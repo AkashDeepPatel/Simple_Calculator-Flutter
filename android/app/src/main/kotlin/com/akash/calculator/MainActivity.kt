b9c5fef1f10f9582f7083340faf0b067be2fd49f
Akash <itsakashdeep148@gmail.com>
2021-04-27 08:45:13 +0530

    CompleteSimpleCalculatorApp

---
+++ android/app/src/main/kotlin/com/akash/calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.akash.calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:calculator/simple_calculator.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SimpleCalculator(),
+    );
+  }
+}
